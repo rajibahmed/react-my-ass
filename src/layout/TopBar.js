8c82c30b9bb9966eefc462dc5b5cdf65ed2b5f49
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-12-31 00:03:20 +0100

    Add classnames to toggling navbar.

--- src/layout/TopBar.js
+++ src/layout/TopBar.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import classNames from 'classnames';
 
-let TopBar = (props) => {
-	return (
-  <div className="hero-head">
-    <header className="nav">
-      <div className="container">
-        <div className="nav-left">
-        </div>
-        <span className="nav-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-        <div className="nav-right nav-menu">
-		{ props.children }
-        </div>
+export default class TopBar  extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = { menuIsActive: false }
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  toggleMenu(){
+    this.setState({
+      menuIsActive: !this.state.menuIsActive
+    })
+  }
+
+  render() {
+    let menuClass = classNames("nav-toggle", {
+      "is-active": this.state.menuIsActive
+    })
+
+    let navItemsClass = classNames("nav-right", "nav-menu", {
+      "is-active": this.state.menuIsActive
+    })
+
+    return (
+      <div className="hero-head">
+        <header className="nav">
+          <div className="container">
+            <div className="nav-left">
+            </div>
+            <span className={menuClass}  onClick={this.toggleMenu} >
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            <div className={navItemsClass} >
+              { this.props.children }
+            </div>
+          </div>
+        </header>
       </div>
-    </header>
-  </div>
-)
+    )
+  }
 }
 
-export default TopBar;
