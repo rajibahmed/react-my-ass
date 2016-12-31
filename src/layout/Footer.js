95e6920819d7f4a749c752297d510df285928328
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-12-31 11:15:09 +0100

    Dumb component.

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default (props) => {
-	return (
-	<div className="hero-foot">
-		<nav className="tabs is-boxed is-fullwidth">
-			<div className="container">
-				friendly footer
-			</div>
-		</nav>
-	</div>
-	);
-}
+export default (props) => (
+  <div className="hero-foot">
+    <nav className="tabs is-boxed is-fullwidth">
+      <div className="container">
+        friendly footer
+      </div>
+    </nav>
+  </div>
+);
